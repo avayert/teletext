3b378a51ac22238a6af7b186e85d04637d8c999d
Ava Yert <ava@yert.pink>
2022-08-14 04:56:33 +0300

    Add barebones keyboard interactivity and a waiting spinner

--- main.py
+++ main.py
@@ -1,28 +1,60 @@
 # NOTE: run this in `python -u` for unbuffered input.
 
 import atexit
-import sys
-import tty
-import termios
-import signal
-import select
 import fcntl
+import itertools
 import os
+import select
+import signal
+import sys
+import termios
+import threading
+import time
+import tty
 
 
 ALT_MODE = '\33[?1049'
-CURSOR = '\33[?25'
-ENABLE = 'h'
-DISABLE = 'l'
+CURSOR   = '\33[?25'
+ENABLE   = 'h'
+DISABLE  = 'l'
 
-CLEAR = '\33[2J'
+CLEAR_BEGIN  = '\33[0J'
+CLEAR_END    = '\33[1J'
+CLEAR_SCREEN = '\33[2J'
+
+UP    = b'\33[A'
+DOWN  = b'\33[B'
+RIGHT = b'\33[C'
+LEFT  = b'\33[D'
+
+CTRL_UP    = b'\33[1;5A'
+CTRL_DOWN  = b'\33[1;5B'
+CTRL_RIGHT = b'\33[1;5C'
+CTRL_LEFT  = b'\33[1;5D'
+
+HOME = '\33[H'
+END  = '\33[F'
+
+PAGE_UP   = '\33[5~'
+PAGE_DOWN = '\33[6~'
+
+# heh, this is BEL
+CTRL_G = '\x07'
 
 c_lflags = 3
 original_termios = None
 original_fcntl = None
 
 
+PAGE_CACHE = {}
 
+
+OFFSET_LEFT = (40 - 22) // 2
+OFFSET_TOP = (40 - 3) // 2
+
+
+def move_cursor_to(*, x, y):
+    output(f'\33[{x};{y}H')
 
 def output(*text):
     sys.stdout.write(''.join(text))
@@ -48,14 +80,16 @@ def initialize_terminal():
     atexit.register(restore_terminal)
 
     output(ALT_MODE, ENABLE)
-    output(CLEAR)
+    output(CLEAR_SCREEN)
     output(CURSOR, DISABLE)
+
+    move_cursor_to(x=0, y=0)
 
 
 def restore_terminal():
-    output(ALT_MODE, DISABLE)
-    # output(CLEAR)
+    output(CLEAR_SCREEN)
     output(CURSOR, ENABLE)
+    output(ALT_MODE, DISABLE)
 
     # `original_X` can't be none because this can't be invoked until `initialize_terminal` has ran
     termios.tcsetattr(sys.stdout.fileno(), termios.TCSANOW, original_termios)
@@ -74,8 +108,47 @@ def input_available():
     # The list is empty if there is nothing to be read, and if there is it's juts gonna be stdin
     return bool(ready_to_read)
 
+def load_page(page):
+    event = threading.Event()
+
+    output(CLEAR_SCREEN)
+
+    def do_request():
+        # TODO: do the actual request, right now I'm just sleeping for austerity
+        time.sleep(2)
+        event.set()
+
+    # TODO: draw a box and a spinner
+    # move_cursor_to(x=OFFSET_LEFT, y=OFFSET_TOP)
+
+    move_cursor_to(x=0, y=0)
+
+    spinner = itertools.cycle(('⢀⠀', '⡀⠀', '⠄⠀', '⢂⠀', '⡂⠀', '⠅⠀', '⢃⠀', '⡃⠀', '⠍⠀', '⢋⠀', '⡋⠀', '⠍⠁', '⢋⠁', '⡋⠁', '⠍⠉', '⠋⠉', '⠋⠉', '⠉⠙', '⠉⠙', '⠉⠩', '⠈⢙', '⠈⡙', '⢈⠩', '⡀⢙', '⠄⡙', '⢂⠩', '⡂⢘', '⠅⡘', '⢃⠨', '⡃⢐', '⠍⡐', '⢋⠠', '⡋⢀', '⠍⡁', '⢋⠁', '⡋⠁', '⠍⠉', '⠋⠉', '⠋⠉', '⠉⠙', '⠉⠙', '⠉⠩', '⠈⢙', '⠈⡙', '⠈⠩', '⠀⢙', '⠀⡙', '⠀⠩', '⠀⢘', '⠀⡘', '⠀⠨', '⠀⢐', '⠀⡐', '⠀⠠', '⠀⢀', '⠀⡀'))
+
+    threading.Thread(target=do_request).start()
+
+    # this busy loops blows but I couldn't think of a better way to do it...
+    while True:
+        if event.is_set():
+            break
+
+        output(CLEAR_END)
+        output(f'Loading page {page:03} {next(spinner)}\r')
+
+        time.sleep(1 / 20)
+
+    return page
+
+
 
 def read_input():
+    page = 0
+
+    output(CLEAR_SCREEN)
+    output(f'Current page is: {page}/10\r\n')
+    output(f'Got command: N/A')
+    move_cursor_to(x=0, y=0)
+
     while True:
         wait_until_input_available()
 
@@ -93,9 +166,26 @@ def read_input():
         # I am genuinely curious why on earth `select` was not returning when there _clearly_ was data in the buffer,
         # since after the next keypress it worked fine. I am really upset about this and I really do want to hear the
         # explanation for it.
+
         command = b''
         while input_available():
             command += os.read(sys.stdin.fileno(), 1)
+
+        if command == LEFT:
+            page = load_page(max(0, page - 1))
+        elif command == RIGHT:
+            page = load_page(min(10, page + 1))
+
+        elif command == CTRL_LEFT:
+            page = load_page(0)
+        elif command == CTRL_RIGHT:
+            page = load_page(10)
+
+        output(CLEAR_SCREEN)
+        output(f'Current page is: {page}/10\r\n')
+        output(f'Got command: {command!r}')
+        move_cursor_to(x=0, y=0)
+
 
         # ESC or C-z
         if command in (b'\x1a', b'\x1b'):
